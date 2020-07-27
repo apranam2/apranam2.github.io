8cbb832976bac83e3a61ab99a5923f524c3a5e41
Abhijit Pranam <abhijitpranam@Space-Sloth.home>
2020-07-26 19:10:31 -0700

    add dates as x axis

--- rookie.js
+++ rookie.js
@@ -6,13 +6,16 @@ async function loadGraph() {
 	var width = 960 - margin.left - margin.right;
 	var height = 500 - margin.top - margin.bottom;
 
+	var minDate = getDate(data[0]);
+	var maxDate = getDate(data[data.length-1]);
+
 	var svg = d3.select("body").append("svg")
 	    .attr("width", width + margin.left + margin.right)
 	    .attr("height", height + margin.top + margin.bottom)
 	    .append("g")
 	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	var x = d3.scaleLinear().domain([0,50]).range([0, width]);
+	var x = d3.scaleTime().domain([minDate, maxDate]).range([0, width]);
 
 	var y = d3.scaleLinear().domain([0,50]).range([height, 0]);
 
@@ -21,7 +24,7 @@ async function loadGraph() {
 	  .enter().append("circle")
 	  .attr("class", "dot")
 	  .attr("r", 3)
-	  .attr("cx", function(d) {return x(d.Minutes);})
+	  .attr("cx", function(d) {return x(d.Dates);})
 	  .attr("cy", function(d) {return y(d.PTS);})
 	  .attr("fill", function(d) { if (d.Result == "L") {return "red";} else {return "blue";}});
 
@@ -32,4 +35,8 @@ async function loadGraph() {
 	d3.select('svg').append('g')
 	.attr('transform','translate(' + margin.left + "," + (height + margin.top) +')')
 	.call(d3.axisBottom(x));
+}
+
+async function getDate(d) {
+        return new Date(d.Dates);
 }
