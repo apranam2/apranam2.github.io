74687d6b7b28f3e7bbfee76bbb01446dc63bc0a2
Abhijit Pranam <abhijitpranam@Space-Sloth.home>
2020-07-29 22:41:39 -0700

    anotha on

--- rookie.js
+++ rookie.js
@@ -34,8 +34,8 @@ async function loadGraph() {
 	  		.style("opacity", 1);
 
 	  	tooltip.html(d.Dates + "</br> Points: " + d.PTS + "</br> Assists: " + d.AST + "</br> Rebounds: " + d.REB + "</br> Steals: " + d.STL)
-	  		.style("left", (d3.event.pageX - 30) + "px")
-        	.style("top", (d3.event.pageY - 30) + "px");
+	  		.style("left", (d3.event.pageX - 60) + "px")
+        	.style("top", (d3.event.pageY - 60) + "px");
 
 	  })
 	  .on("mouseout", function(d) {
