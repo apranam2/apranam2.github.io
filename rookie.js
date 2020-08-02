b4a773d2e9009c6270f5c6e0b848c4bcc2ca19f0
Abhijit Pranam <abhijitpranam@Space-Sloth.home>
2020-08-02 14:58:48 -0700

    rookie anno edit

--- rookie.js
+++ rookie.js
@@ -61,16 +61,16 @@ async function loadGraph() {
     .attr("fill", "black");
 
     svg.append("line")
-    .attr("x1", 835)
+    .attr("x1", 800)
     .attr("y1", 70)
-    .attr("x2", 855)
+    .attr("x2", 820)
 	.attr("y2", 70)
 	.attr("stroke", "black")
 	.attr("stroke-width", 1);
 
 	svg.append("text")
-    .attr("x", 865)
-    .attr("y", 70)
+    .attr("x", 750)
+    .attr("y", 72)
 	.text("Season High of 42 points")
 	.attr("font-family", "sans-serif")
     .attr("font-size", "12px")
